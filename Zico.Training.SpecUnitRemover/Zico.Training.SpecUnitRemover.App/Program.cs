19acfb374cbde7976e92372a19a155c579248095
Raphael Yoshiga <Raphael.Yoshiga@asos.com>
2016-05-05 16:38:16 +0100

    Configure program.cs

--- Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover.App/Program.cs
+++ Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover.App/Program.cs
@@ -10,8 +10,21 @@ namespace Zico.Training.SpecUnitRemover.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            string solutionPath = "C:\\Test\\";
+            Console.WriteLine("Starting to remove SpecUnit from: {0}", solutionPath);
+
+            InstanciateRemoverOrchestrator().RemoveSpecUnitFromTests(solutionPath);
+
+            Console.WriteLine("Finished removing spec unit");
             Console.Read();
+        }
+
+        private static SpecUnitRemoverOrchestrator InstanciateRemoverOrchestrator()
+        {
+            SpecUnitFileTransformer fileTranformer = new SpecUnitFileTransformer(new DotNetFileReader(), new ContentRemover(),
+                new DotNetFileWriter());
+            var orchestrator = new SpecUnitRemoverOrchestrator(new CSharpFilesProvider(), fileTranformer);
+            return orchestrator;
         }
     }
 }
