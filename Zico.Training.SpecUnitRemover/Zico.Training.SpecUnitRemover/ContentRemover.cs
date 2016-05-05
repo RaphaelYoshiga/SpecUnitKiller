e8d0b3f9bc8d028b9878a5d30f6346409c9d04c2
rebeccaL8 <rebeccal_18@hotmail.co.uk>
2016-05-05 11:55:14 +0100

    Removing and replacing When and And statements

--- Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover/ContentRemover.cs
+++ Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover/ContentRemover.cs
@@ -4,10 +4,43 @@
     {
         public string Remove(string parse)
         {
+            var removeGiven = RemoveGiven(parse);
+
+            var removeWhen = RemoveWhen(removeGiven);
+
+            return RemoveAnd(removeWhen);
+        }
+
+        private static string RemoveAnd(string removeWhen)
+        {
+            var whenIndex = removeWhen.IndexOf(".And(");
+            if (whenIndex > 0)
+                return UpdateString(removeWhen, whenIndex, 5);
+            return removeWhen;
+        }
+
+        private static string RemoveWhen(string removeGiven)
+        {
+            var whenIndex = removeGiven.IndexOf(".When(");
+            if (whenIndex > 0)
+                return UpdateString(removeGiven, whenIndex, 6);
+            return removeGiven;
+        }
+
+        private static string RemoveGiven(string parse)
+        {
             var givenIndex = parse.IndexOf("Given(");
-            string noGivenString = parse.Remove(givenIndex, 6);
-            int parenthesisIndex = noGivenString.IndexOf(")", givenIndex);
-            return noGivenString.Insert(parenthesisIndex, "(").Insert(parenthesisIndex + 2, ";");
+            if (givenIndex > 0)
+                return UpdateString(parse, givenIndex, 6);
+            return parse;
+        }
+
+        private static string UpdateString(string parse, int index, int startIndex)
+        {
+            string remove = parse.Remove(index, startIndex);
+            int parenthesisIndex = remove.IndexOf(")", index);
+            var result = remove.Insert(parenthesisIndex, "(").Insert(parenthesisIndex + 2, ";");
+            return result;
         }
     }
 }
--- Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover/ContentRemoverShould.cs
+++ Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover/ContentRemoverShould.cs
@@ -6,26 +6,10 @@ namespace Zico.Training.SpecUnitRemover
     [TestFixture]
     public class ContentRemoverShould
     {
-        private string parse =@"[Test]
-        public void Collaborate_with_matflo_api_to_send_heartbeat_only_when_no_previous_heartbeat_sent()
-        {
-            Given(i_have_an_instance_of_matflo)
-                .When(i_send_a_heartbeat_request)
-                .And(i_send_a_heartbeat_request)
-                .Then(we_only_call_matfloApi_0_, 1, ""heartbeat"");
-        }";
-
-        private string expected = @"[Test]
-        public void Collaborate_with_matflo_api_to_send_heartbeat_only_when_no_previous_heartbeat_sent()
-        {
-            i_have_an_instance_of_matflo();
-                .When(i_send_a_heartbeat_request)
-                .And(i_send_a_heartbeat_request)
-                .Then(we_only_call_matfloApi_0_, 1, ""heartbeat"");
-        }";
-
-        [Test]
-        public void ReplaceGiven()
+        [TestCase(" Given(i_have_an_instance_of_matflo)", " i_have_an_instance_of_matflo();")]
+        [TestCase(" .When(i_send_a_heartbeat_request)", " i_send_a_heartbeat_request();")]
+        [TestCase(" .And(i_send_a_heartbeat_request)", " i_send_a_heartbeat_request();")]
+        public void ReplaceGiven(string parse, string expected)
         {
             ContentRemover remover = new ContentRemover();
 
@@ -33,5 +17,11 @@ namespace Zico.Training.SpecUnitRemover
 
             result.Should().Be(expected);
         }
+
+        private const string intialString = @"
+            Given(i_have_an_instance_of_matflo)
+                .When(i_send_a_heartbeat_request)
+                .And(i_send_a_heartbeat_request)
+                .Then(we_only_call_matfloApi_0_, 1, ""heartbeat"");";
     }
 }
