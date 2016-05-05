c1a74644fd673f87b99e19d3942797f152236559
rebeccaL8 <rebeccal_18@hotmail.co.uk>
2016-05-05 11:11:03 +0100

    New contentRemover Class and initial test

---
+++ Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover/ContentRemover.cs
@@ -0,0 +1,13 @@
+﻿namespace Zico.Training.SpecUnitRemover
+{
+    class ContentRemover
+    {
+        public string Remove(string parse)
+        {
+            var index = parse.IndexOf("Given(");
+            var foo = parse.Remove(index, 6);
+
+            //foo.IndexOf()
+        }
+    }
+}
---
+++ Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover/ContentRemoverShould.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Zico.Training.SpecUnitRemover
+{
+    [TestFixture]
+    class ContentRemoverShould
+    {
+        private string parse =@"[Test]
+        public void Collaborate_with_matflo_api_to_send_heartbeat_only_when_no_previous_heartbeat_sent()
+        {
+            Given(i_have_an_instance_of_matflo)
+                .When(i_send_a_heartbeat_request)
+                .And(i_send_a_heartbeat_request)
+                .Then(we_only_call_matfloApi_0_, 1, ""heartbeat"");
+        }";
+
+        private string expected = @"[Test]
+        public void Collaborate_with_matflo_api_to_send_heartbeat_only_when_no_previous_heartbeat_sent()
+        {
+            i_have_an_instance_of_matflo();
+                .When(i_send_a_heartbeat_request)
+                .And(i_send_a_heartbeat_request)
+                .Then(we_only_call_matfloApi_0_, 1, ""heartbeat"");
+        }";
+
+        [Test]
+        public void ReplaceGiven()
+        {
+            ContentRemover remover = new ContentRemover();
+            var result = remover.Remove(parse);
+            result.Should().Be(expected);
+        }
+    }
+}
