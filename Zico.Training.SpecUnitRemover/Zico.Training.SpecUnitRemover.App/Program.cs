441edefcb7354be197e6fcc3503f74480a1edcf7
Raphael Yoshiga <Raphael.Yoshiga@asos.com>
2016-05-05 13:49:54 +0100

    Change project to

---
+++ Zico.Training.SpecUnitRemover/Zico.Training.SpecUnitRemover.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zico.Training.SpecUnitRemover.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.Read();
+        }
+    }
+}
